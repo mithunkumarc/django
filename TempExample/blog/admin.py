b0c3a3972b89a4c6af73e34d9fed53e09449dbe6
mithunkumarc <mithunchannappa@gmail.com>
2018-09-04 18:25:02 +0530

    Update admin.py

--- TempExample/blog/admin.py
+++ TempExample/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+admin.site.register(Post)
+
+
+
+# admin interface will directly interact with blog model(Post)/blog_post table
+#open localhost:8000/admin : 
+#you will find blog project title under this, you will find a link to interact with blog_post table
