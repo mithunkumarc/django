5377deeca6f958a123ca458fe2a7e7ed7a824eb1
mithunkumarc <mithunchannappa@gmail.com>
2018-09-06 12:18:25 +0530

    Update form.py

--- ModelFormExample/studentapp/form.py
+++ ModelFormExample/studentapp/form.py
@@ -4,6 +4,7 @@ from studentapp.models import Student
 
 #using model forms
 #forms.Form for django forms
+#forms.Form for model form
 class StuForm(forms.ModelForm):
     class Meta:
         model = Student
