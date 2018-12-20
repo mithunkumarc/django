d042c58e2fb8bfd601ac0f30987e32120efccbfa
mithunkumarc <mithunchannappa@gmail.com>
2018-12-20 16:34:49 +0530

    Delete views.py

--- new-examples/4. forms/student/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render  
-from student.form import StuForm  
-      
-def index(request):  
-    stu = StuForm()  
-    return render(request,"index.html",{'form':stu})  
