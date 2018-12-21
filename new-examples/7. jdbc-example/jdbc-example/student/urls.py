bb7e622309769c217fe4df3973ce18601e7ce4ec
mithunkumarc <mithunchannappa@gmail.com>
2018-12-21 19:15:33 +0530

    Add files via upload

---
+++ new-examples/7. jdbc-example/jdbc-example/student/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from student.views import index,create
+
+urlpatterns = [
+    path('index/', index, name='index'),
+    path('student_create/', create, name='create'),
+] 
---
+++ new-examples/7. jdbc-example/jdbc-example/student/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render,redirect  
+from student.form import StuForm  
+from django.http import HttpResponse  
+from django.views.decorators.http import require_http_methods  
+
+def index(request):  
+    stu = StuForm()  
+    return render(request,"index.html",{'form':stu})
+
+
+@require_http_methods(["POST"])  
+def create(request):
+    if request.method == "POST":  
+        form = StuForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save()  
+                return redirect('/student/index')  
+            except:  
+                pass  
+    else:  
+        form = StuForm()  
+    return render(request,'index.html',{'form':form})  
